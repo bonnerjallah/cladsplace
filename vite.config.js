09b28d3772fc9ea047c6c16f5ba919dccd03c38a
bonnerjallah <121054718+bonnerjallah@users.noreply.github.com>
2024-01-15 22:27:38 -0500

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: isProduction ? '/' : '/',
+  base: '/cladsplace',
   plugins: [react()],
   
 })
